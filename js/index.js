d6e0c79691c43c0e1c6d8613a8ad773feb22e8b8
Thr3e <tony_thr3e@163.com>
2018-06-09 14:58:10 +0800

    更新文档结构，添加注释

--- js/index.js
+++ js/index.js
@@ -1,32 +1,21 @@
 $(function () {
-    var links = [
-        './pages/today.html',
-        './pages/collection.html',
-        './pages/news.html',
-        './pages/more.html'
-    ];
     var curIndex;
     $('.page-ctrl-btn').on('click', function (e) {
         var index = $(this).index() / 2;
+        //判断点击页是否和当前页相同，避免重复点击
         if (index === curIndex) return;
         curIndex = index;
         $.ajax({
-            url: links[curIndex],
+            url: page_links[curIndex].link,
             type: 'GET',
             success: function (response) {
                 $('#content').html(response);
+                //通过字符串调用函数
+                eval(page_links[curIndex].func + '()');
             }
         })
-        switch (curIndex) {
-            case 0 : todayFuncs();break;
-            case 1 : collectionFuncs();break;
-            case 2 : newsFuncs();break;
-            case 3 : moreFuncs();break;
-        }
     // });
     }).first().trigger('click');
-
     $('.topright_btn').children().click(loadLoginPage);
-
     $('.topleft_btn').children().click(loadSearchPage);
 });
--- js/tool.js
+++ js/tool.js
@@ -1,74 +1,42 @@
-function getFullLocalHour (time) {
-    return time.slice(0, 1) === '下' ? parseInt(time.slice(2, time.length - 3)) + 12 + time.slice(time.length - 6, time.length - 3) : time.slice(2, time.length - 3);
+//----------BOOL----------
+/**
+ * @description 判断当前是否是晚上（20点至次日6点）
+ * @returns Boolean true:是晚上
+ */
+function isNowNight () {
+    var d = new Date().toLocaleTimeString();
+    return (d.match(/下午/) && parseInt(d.slice(2)) > 7) || (d.match(/上午/) && (parseInt(d.slice(2)) < 7) || parseInt(d.slice(2)) === 12);
 }
-
-function getLocationData (url){
-    $.ajax({
-        url: 'http://api.map.baidu.com/' + url,
-        type: 'GET',
-        data: {
-            'ak' : '7ZlcHGoo1hUOedvlmVfKk6TNAG7vyBtG',
-            'coor' : 'gcj02'
-        },
-        dataType:'jsonp',
-        success:function(response){ xmlData[url] = response['address'].split('|'); }
-    })
-}
-
-function getWeatherData (url, location){
-    $.ajax({
-        url: `https://free-api.heweather.com/s6/${url}?parameters`,
-        type: 'GET',
-        data: {
-            'key':'d435517aad8148dfa6d1fd29a52a47e7',
-            'location' : location
-        },
-        dataType:'json',
-        success:function(response){ xmlData[url] = response['HeWeather6']['0']; }
-    })
-}
-
-function setXmlData (location){
-    jQuery.each(xmlData,function(key, val){ xmlData[key] = null; });
-    if (location) xmlData['location/ip'] = location; 
-    else getLocationData('location/ip');
-
-    var t = setInterval(function(){
-        if (xmlData['location/ip']) {
-            getWeatherData('air/now', xmlData['location/ip'][2]);
-            getWeatherData('weather', xmlData['location/ip'][2]);
-            clearInterval(t);
-            t = null;
-        }
-    },300);
-}
-
+//----------LOAD----------
+/**
+ * @description 加载today页面
+ */
 function loadTodayView (){
-    var todayInfo  = xmlData['weather']['daily_forecast'][0],
-    aqiNowInfo = xmlData['air/now']['air_now_city'];
+    var todayInfo  = xmlData['weather']['daily_forecast'][0];
     $('#title').find('.city').text(`${xmlData['location/ip'][2]}市`);   
     $('#title').find('.province').text(`${xmlData['location/ip'][1]}省`);
     $('.today-weather').text(`${todayInfo['tmp_min']}-${todayInfo['tmp_max']}℃`);
-    $('.quality_info').text(`${aqiNowInfo.aqi}&nbsp空气质量${aqiNowInfo.qlty}`);
-    $('.quality_info').css({'background': `${colorArr[parseInt(aqiNowInfo.aqi / 50)]}`});
-
-    loadMainview(xmlData['weather']);
-    loadDetailview(xmlData['weather'], xmlData['air/now']);
+    loadMainView();
+    loadDetailView();
 }
 
-function loadMainview (dataInfo) {
+/**
+ * @description 加载today页面中的mainView
+ */
+function loadMainView () {
     var d = new Date(),
-        todayInfo = dataInfo['daily_forecast'][0],
-        nowInfo = dataInfo['now'],
+        todayInfo = xmlData['weather']['daily_forecast'][0],
+        nowInfo = xmlData['weather']['now'],
+        aqiNowInfo = xmlData['air/now']['air_now_city'],
         curIndex = 0;
     
     var curHtml = [{
-        'icon' : getWeatherImgUrl(dataInfo['daily_forecast'][0]),
+        'icon' : getWeatherImgUrl(xmlData['weather']['daily_forecast'][0]),
         'time' : "今天·周" + "日一二三四五六".charAt(d.getDay()),
         'weather' : todayInfo['tmp_min'] + '-' + todayInfo['tmp_max'] + '℃',
         'text' : isNowNight() ? todayInfo['cond_txt_n'] : todayInfo['cond_txt_d']
     },{
-        'icon' : getWeatherImgUrl(dataInfo['now']),
+        'icon' : getWeatherImgUrl(xmlData['weather']['now']),
         'time' : "现在·" + getFullLocalHour(d.toLocaleTimeString()),
         'weather' : nowInfo['tmp'] + '℃',
         'text' : nowInfo['cond_txt']
@@ -78,6 +46,8 @@ function loadMainview (dataInfo) {
     $('.time_info').text(curHtml[curIndex]['time']);
     $('.degree_info').text(curHtml[curIndex]['weather']);
     $('.weather_info').text(curHtml[curIndex]['text']);
+    $('.quality_info').text(`${aqiNowInfo.aqi}&nbsp空气质量${aqiNowInfo.qlty}`);
+    $('.quality_info').css({'background': `${colorArr[parseInt(aqiNowInfo.aqi / 50)]}`});
     var t = setInterval(function(){
         if (curIndex === 0) curIndex = 1;
         else curIndex = 0;
@@ -88,10 +58,15 @@ function loadMainview (dataInfo) {
     },4000);
 }
 
-function loadDetailview (weatherData, airData) {
-    var htmlStr = $('#today_detailview').html();
+/**
+ * @description 加载today页面中的detailview
+ */
+function loadDetailView () {
+    var htmlStr = $('#today_detailview').html(),
+        weatherData = xmlData['weather'],
+        airData = xmlData['air/now'];
 
-    htmlStr +=   joinHtmlString (weatherData['update']['loc'].slice(11),
+    htmlStr +=   setHtmlString (weatherData['update']['loc'].slice(11),
                                 `${getWeatherIcon(weatherData['now']['cond_code'])}`,
                                 `${weatherData['now']['cond_txt']}&nbsp${weatherData['now']['tmp']}℃`,
                                 `<table><tr>
@@ -119,7 +94,7 @@ function loadDetailview (weatherData, airData) {
                                     </td>
                                 </tr></table>`
                                 );
-    htmlStr +=  joinHtmlString  ("",
+    htmlStr +=  setHtmlString  ("",
                                 `${getAirIcon(airData)}`,
                                 `空气指数:&nbsp${airData['air_now_city']['aqi']}&nbsp${airData['air_now_city']['qlty']}`,
                                 `<table><tr>
@@ -133,7 +108,7 @@ function loadDetailview (weatherData, airData) {
                                     <td>O3&nbsp${airData['air_now_city']['o3']}</td>
                                 </tr></table>`
                                 );                            
-    htmlStr +=  joinHtmlString  ("",
+    htmlStr +=  setHtmlString  ("",
                                 'icon-aixin',
                                 `生活指数`,
                                 `<table><tr>
@@ -149,12 +124,12 @@ function loadDetailview (weatherData, airData) {
                                     <td>交通&nbsp${weatherData['lifestyle'][4]['brf']}</td>
                                 </tr></table>`
                                 );
-    htmlStr +=  joinHtmlString  ("",
+    htmlStr +=  setHtmlString  ("",
                                 'icon-hangche',
                                 `今日限行尾号:&nbsp${getLimitNum(new Date().getDay())}`,
                                 `明日现行尾号:&nbsp${getLimitNum(new Date().getDay() + 1)}`
                                 );
-    htmlStr += joinHtmlString   ('明天',
+    htmlStr += setHtmlString   ('明天',
                                 `${getWeatherIcon(weatherData['daily_forecast'][1]['cond_code_d'])}`,
                                 `${weatherData['daily_forecast'][1]['cond_txt_d']}&nbsp${weatherData['daily_forecast'][1]['tmp_min'] + '-' + weatherData['daily_forecast'][1]['tmp_max'] + '℃'}`,
                                 `<table><tr>
@@ -166,7 +141,7 @@ function loadDetailview (weatherData, airData) {
                                     <td>湿度&nbsp${weatherData['daily_forecast'][1]['hum']}</td>
                                 </tr></table>`
                                 );
-    htmlStr += joinHtmlString   ('后天',
+    htmlStr += setHtmlString   ('后天',
                                 `${getWeatherIcon(weatherData['daily_forecast'][2]['cond_code_d'])}`,
                                 `${weatherData['daily_forecast'][2]['cond_txt_d']}&nbsp${weatherData['daily_forecast'][2]['tmp_min'] + '-' + weatherData['daily_forecast'][2]['tmp_max'] + '℃'}`,
                                 `<table><tr>
@@ -181,7 +156,87 @@ function loadDetailview (weatherData, airData) {
     $('#today_detailview').html(htmlStr);
 }
 
-function joinHtmlString(title, attr, content, detail) {
+/**
+ * @description 加载登录页面
+ */
+function loadLoginPage(){
+    $.ajax({
+        url : './pages/log-reg.html',
+        type : 'GET',
+        success : function (response){
+            $('#login_page').html(response).css('left', '0');
+            setLogPage();
+            $('.log_btn').click(setLogFunc);
+            $('.reg_btn').click(setRegFunc);
+        }
+    })
+}
+
+/**
+ * @description 加载搜索页面
+ */
+function loadSearchPage () {
+    $.ajax({
+        url : './pages/search.html',
+        type : 'GET',
+        success : function (response){ 
+            $('#search_page').html(response).css('left','0'); 
+            $('.search_back_icon').click(function(){
+                $('#search_page').css('left', '-70%');
+                setTimeout(function(){$('#search_page').html(null)},500);
+            });
+        }
+    });
+    // var curCity = [],
+    //     chooseCity = [],
+    //     hotCity = ['重庆','香港','上海','北京','深圳','武汉','成都','杭州','澳门','苏州'];
+    // getLocationData ('location/ip', function (response) {
+    //     curCity.push(response['address'].split('|')[2]); 
+    //     setCityBtn(curCity, '.locate .btn_wrap');
+    // });
+    // City_Info.forEach(function(val, idx, arr){
+    //     chooseCity.push(val['province'].slice(0,val['province'].indexOf(',')));
+    // })
+    // setCityBtn(chooseCity, '.choose .btn_wrap');
+    // setCityBtn(hotCity, '.hot .btn_wrap');
+    // $('.locate .city_btn, .hot .city_btn').click(function(){
+    //     // $('#search_page').css('left', '-70%');
+    //     console.log(2);
+    // })
+    // $('.hot .city_btn').click(function(){
+    //     // $('#search_page').css('left', '-70%');
+    //     console.log(1);
+    // });
+}
+//----------SET-----------
+/**
+ * @description 获取地址、天气、空气质量信息并赋值给xmlData
+ * @param {*} location 需要查询的地址，忽略则取用当前IP所在地址
+ */
+function setXmlData (location){
+    jQuery.each(xmlData,function(key, val){ xmlData[key] = null; });
+    if (location) xmlData['location/ip'] = location; 
+    else getLocationData('location/ip');
+
+    var t = setInterval(function(){
+        if (xmlData['location/ip']) {
+            getWeatherData('air/now', xmlData['location/ip'][2]);
+            getWeatherData('weather', xmlData['location/ip'][2]);
+            clearInterval(t);
+            t = null;
+        }
+    },300);
+}
+
+/**
+ * 
+ * @description 拼接detailview板块的各个子版块信息
+ * @param {*} title  子版块主标题
+ * @param {*} attr   子版块副标题的icon图表类名
+ * @param {*} content 子版块副标题
+ * @param {*} detail 子版块主要内容
+ */
+function setHtmlString(title, attr, content, detail) {
     return `
     <div class="detailContent">
         <div class="title">
@@ -200,12 +255,138 @@ function joinHtmlString(title, attr, content, detail) {
     `;
 }
 
+/**
+ * @description 设置登录页相关功能
+ */
+function setLogPage (){
+    setTipsClear();
+    $('.login_wrap').css('height', '100px');
+    $('.logo').css({'width':'256px', 'height':'256px', 'margin':'72px auto'});
+    $('.log_btn').css('display', 'block');
+    $('.reg_btn').css('display', 'none');
+    $('.tr_btn').children().click(setRegPage);
+    $('.tl_btn').children().click(function() {
+        $('#login_page').css('left', '100%');
+        setTimeout(function(){$('#login_page').html(null)}, 500);
+    });
+}
+
+/**
+ * @description 设置注册页相关功能
+ */
+function setRegPage (){
+    $('.login_wrap').css('height', '450px');
+    $('.logo').css({'width':'50px', 'height':'50px', 'margin':'0 auto'});
+    $('.user_name').on('blur', function(){getUserInfoVerify($(this), /^\w{6,20}$/)});
+    $('.user_pwd').on('blur', function(){getUserInfoVerify($(this), /^[A-Z].{5,}$/)});
+    $('.conf_pwd').on('blur', function(){getUserInfoVerify($(this), $('.user_pwd').val())});
+    $('.user_email').on('blur', function(){getUserInfoVerify($(this), /^.?@\w+\.\w+$/)});
+    $('.user_tel').on('blur', function(){getUserInfoVerify($(this), /^1\d{10}$/)});
+    $('.verf_code').on('blur', function(){getUserInfoVerify($(this), $('.verif_text').text())});
+    $('.log_btn').css('display', 'none');
+    $('.reg_btn').css('display', 'block');
+    $('.verif_text').text(getVerificationCode(4)).click(function(){
+        $(this).text(getVerificationCode(4));
+    }); 
+    $(this).click(setLogPage);
+}
+
+/**
+ * @description 将城市信息加载到sel页面上
+ * @param {Array} arr 城市信息数组
+ * @param {String} sel 待加载页面选择器
+ */
+function setCityBtn(arr, sel) {
+    var htmlStr = '';
+    arr.forEach(function(val, idx, arr) {
+        htmlStr += `<div class="city_btn">${val}</div>`;
+    })
+    $(sel).html(htmlStr);
+}
+
+/**
+ * @description 清除注册页面错误提示信息
+ */
+function setTipsClear (){
+    $('.tips').css('opacity', '0');
+    $('.wrong_tag').css('right', '50px');
+}
+
+/**
+ * @description 注册按钮功能逻辑
+ */
+function setRegFunc (){
+    $('.user_ipt').trigger('blur');
+    $('.topleft_btn').children().attr({ class : 'iconfont icon-zhuce' })
+}
+
+/**
+ * @description 登录按钮功能逻辑
+ */
+function setLogFunc (){
+    console.log(1);
+    // TODO:
+}
+//----------GET-----------
+/**
+ * @description 调用百度地图api获取当前地址，并赋值给xmlDdta对象
+ * @param {*} url api接口
+ */
+function getLocationData (url){
+    $.ajax({
+        url: 'http://api.map.baidu.com/' + url,
+        type: 'GET',
+        data: {
+            'ak' : '7ZlcHGoo1hUOedvlmVfKk6TNAG7vyBtG',
+            'coor' : 'gcj02'
+        },
+        dataType:'jsonp',
+        success:function(response){ xmlData[url] = response['address'].split('|'); }
+    })
+}
+
+/**
+ * @description 调用和风天气api获取当前天气或空气质量，并赋值给xmlDdta对象
+ * @param {*} url api接口
+ */
+function getWeatherData (url, location){
+    $.ajax({
+        url: `https://free-api.heweather.com/s6/${url}?parameters`,
+        type: 'GET',
+        data: {
+            'key':'d435517aad8148dfa6d1fd29a52a47e7',
+            'location' : location
+        },
+        dataType:'json',
+        success:function(response){ xmlData[url] = response['HeWeather6']['0']; }
+    })
+}
+
+/**
+ * @description 获得当日限号
+ * @param {Number} day 星期数
+ * @returns 当日限号号数
+ */
 function getLimitNum (day) {
     day %= 7;
     return (day === 0) || (day === 6) ? "不限行" : `${day} & ${day === 5 ? 0 : day + 5}`;
 }
 
-function verifyUserInfo(el, regx){
+/**
+ * @description 获取24小时制时间
+ * @param {Date} time 时间
+ * @returns 24小时制时间
+ */
+function getFullLocalHour (time) {
+    return time.slice(0, 1) === '下' ? parseInt(time.slice(2, time.length - 3)) + 12 + time.slice(time.length - 6, time.length - 3) : time.slice(2, time.length - 3);
+}
+
+/**
+ * @description 监测用户输入信息是否合法
+ * @param {Document} el input输入框对象
+ * @param {RegExp} regx 正则表达式
+ */
+function getUserInfoVerify(el, regx){
     if (el.val() && el.val().match(regx)){
         el.siblings('.tips').css('opacity', '0');
         el.siblings('.wrong_tag').css('right', '50px');
@@ -215,58 +396,11 @@ function verifyUserInfo(el, regx){
     }
 }
 
-function clearTips (){
-    $('.tips').css('opacity', '0');
-    $('.wrong_tag').css('right', '50px');
-}
-
-function regFunc (){
-    $('.user_ipt').trigger('blur');
-    $('.topleft_btn').children().attr({ class : 'iconfont icon-zhuce' })
-}
-
-function logFunc (){
-    console.log(1);
-    // TODO:
-}
-
-function loadLoginPage(){
-    $('#login_page').css('left', '0');
-    setLogPage();
-    $('.log_btn').click(logFunc);
-    $('.reg_btn').click(regFunc);
-}
-
-function setLogPage (){
-    clearTips();
-    $('.login_wrap').css('height', '100px');
-    $('.logo').css({'width':'256px', 'height':'256px', 'margin':'72px auto'});
-    $('.log_btn').css('display', 'block');
-    $('.reg_btn').css('display', 'none');
-    $('.tr_btn').children().click(setRegPage);
-    $('.tl_btn').children().click(function() {
-        $('#login_page').css('left', '100%');
-        setLogPage();
-    });
-}
-
-function setRegPage (){
-    $('.login_wrap').css('height', '450px');
-    $('.logo').css({'width':'50px', 'height':'50px', 'margin':'0 auto'});
-    $('.user_name').on('blur', function(){verifyUserInfo($(this), /^\w{6,20}$/)});
-    $('.user_pwd').on('blur', function(){verifyUserInfo($(this), /^[A-Z].{5,}$/)});
-    $('.conf_pwd').on('blur', function(){verifyUserInfo($(this), $('.user_pwd').val())});
-    $('.user_email').on('blur', function(){verifyUserInfo($(this), /^.?@\w+\.\w+$/)});
-    $('.user_tel').on('blur', function(){verifyUserInfo($(this), /^1\d{10}$/)});
-    $('.verf_code').on('blur', function(){verifyUserInfo($(this), $('.verif_text').text())});
-    $('.log_btn').css('display', 'none');
-    $('.reg_btn').css('display', 'block');
-    $('.verif_text').text(getVerificationCode(4)).click(function(){
-        $(this).text(getVerificationCode(4));
-    }); 
-    $(this).click(setLogPage);
-}
-
+/**
+ * @description 通过天气代码获取对应图片url
+ * @param {Object} obj 天气信息对象
+ * @returns string 图片url 
+ */
 function getWeatherImgUrl (obj) {
     var endStr = '.png',
         tmpArr = ['100', '103', '104', '300', '301', '406', '407'];
@@ -285,11 +419,11 @@ function getWeatherImgUrl (obj) {
     }
 }
 
-function isNowNight () {
-    var d = new Date().toLocaleTimeString();
-    return (d.match(/下午/) && parseInt(d.slice(2)) > 7) || (d.match(/上午/) && (parseInt(d.slice(2)) < 7) || parseInt(d.slice(2)) === 12);
-}
-
+/**
+ * @description 获取天气图标
+ * @param {Object} obj 天气信息对象
+ * @returns 天气图标类名
+ */
 function getWeatherIcon (obj){
     var iconName = 'icon-unknown'
     WeatherIconRullArr.forEach(function(val, idx, arr){
@@ -298,52 +432,13 @@ function getWeatherIcon (obj){
     return iconName;
 }
 
+/**
+ * @description 获取空气质量图标
+ * @param {Object} obj 空气信息对象
+ * @returns 空气图标类名
+ */
 function getAirIcon (obj) {
     var rate = parseInt(obj['air_now_city'].aqi / 50);
     var iconArr = ['icon-laugh','icon-weixiao','icon-kaixin','icon-face','icon-unhappy','icon-angry'];
     return iconArr[rate];
-}
-
-function loadSearchPage () {
-    $('#search_page').css('left','0');
-    $('.search_back_icon').click(function(){$('#search_page').css('left', '-70%')});
-    var curCity = [],
-        chooseCity = [],
-        hotCity = ['重庆','香港','上海','北京','深圳','武汉','成都','杭州','澳门','苏州'];
-    getLocationData ('location/ip', function (response) {
-        curCity.push(response['address'].split('|')[2]); 
-        setCityBtn(curCity, '.locate .btn_wrap');
-    });
-    City_Info.forEach(function(val, idx, arr){
-        chooseCity.push(val['province'].slice(0,val['province'].indexOf(',')));
-    })
-    setCityBtn(chooseCity, '.choose .btn_wrap');
-    setCityBtn(hotCity, '.hot .btn_wrap');
-    $('.locate .city_btn, .hot .city_btn').click(function(){
-        var location = $(this).text();
-        getWeatherData ('weather', location, function (response) {
-            var wthData = response['HeWeather6']['0'],
-                todayInfo = wthData['daily_forecast'][0];
-            loadMainview (wthData);
-            $('.today-weather').text(todayInfo['tmp_min'] + '-' + todayInfo['tmp_max'] + '℃');
-        });
-        getWeatherData ('air/now', location, function (response) {
-            // airData = response['HeWeather6']['0'];
-            // setTimeout(loadDetailview, 1000);
-            // console.log(AirData);
-            // var aqi = parseInt(AirData['air_now_city']['aqi']);
-            // $('.quality_info').text(aqi + ' 空气质量' + AirData['air_now_city']['qlty']);
-            // $('.quality_info').css({'background': `${colorArr[parseInt(aqi / 50)]}`});
-        });
-        $('#search_page').css('left', '-70%');
-    })
-
-}
-
-function setCityBtn(arr, sel) {
-    var htmlStr = '';
-    arr.forEach(function(val, idx, arr) {
-        htmlStr += `<div class="city_btn">${val}</div>`;
-    })
-    $(sel).html(htmlStr);
 }
